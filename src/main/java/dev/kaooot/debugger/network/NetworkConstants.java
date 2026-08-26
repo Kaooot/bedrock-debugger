d20c01761ba705df727f50ba7209aca8709ca001
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-08-26 16:29:25 +0200

    Update to codec v2192 (1.26.50-beta.26)

--- src/main/java/dev/kaooot/debugger/network/NetworkConstants.java
+++ src/main/java/dev/kaooot/debugger/network/NetworkConstants.java
@@ -2,7 +2,7 @@ package dev.kaooot.debugger.network;
 
 import lombok.experimental.UtilityClass;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v2187.Bedrock_v2187;
+import org.cloudburstmc.protocol.bedrock.codec.v2192.Bedrock_v2192;
 import org.cloudburstmc.protocol.bedrock.data.EncodingSettings;
 
 /**
@@ -13,6 +13,6 @@ import org.cloudburstmc.protocol.bedrock.data.EncodingSettings;
 @UtilityClass
 public class NetworkConstants {
 
-    public final BedrockCodec CODEC = Bedrock_v2187.CODEC;
+    public final BedrockCodec CODEC = Bedrock_v2192.CODEC;
     public final EncodingSettings ENCODING_SETTINGS = EncodingSettings.UNLIMITED;
 }
